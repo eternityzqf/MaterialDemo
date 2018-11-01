48a4f810fcda3fe00a3ba371bbfdd4899d193950
eternityzqf <1583510917@qq.com>
2018-11-01 15:28:49 +0800

    add CollapsingToolbarLayout

---
+++ app/src/main/java/com/zqf/materialdemo/MaterialViewPager2.java
@@ -0,0 +1,56 @@
+package com.zqf.materialdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author：zqf
+ * Time：2018/10/31 15:45
+ * desc：
+ */
+public class MaterialViewPager2 extends AppCompatActivity {
+    ViewPager mViewPager;
+    List<Fragment> mFragments;
+    String[] mTitles = new String[]{"主页", "微博", "相册"};
+    private TabLayout mTabLayout;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_viewpagerlayout2);
+        // 第一步，初始化ViewPager和TabLayout
+        mViewPager = findViewById(R.id.viewpager);
+        mTabLayout = findViewById(R.id.tabs);
+        mToolbar=  findViewById(R.id.toolbar);
+        mToolbar.setTitle("唐嫣");
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setupViewPager();
+    }
+
+    private void setupViewPager() {
+
+        mFragments = new ArrayList<>();
+        for (int i = 0; i < mTitles.length; i++) {
+            ListFragment listFragment = ListFragment.newInstance(mTitles[i]);
+            mFragments.add(listFragment);
+        }
+        // 第二步：为ViewPager设置适配器
+        BaseFragmentAdapter adapter = new BaseFragmentAdapter(getSupportFragmentManager(), mFragments, mTitles);
+
+        mViewPager.setAdapter(adapter);
+        //  第三步：将ViewPager与TableLayout 绑定在一起
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+}
